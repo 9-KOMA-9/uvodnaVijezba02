f3e2b9439a637a79ace9890e02e000174d90601b
9-KOMA-9 <145533890+9-KOMA-9@users.noreply.github.com>
2023-09-20 09:02:25 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uvodnaViježba02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            double c, d;
+            a = 10;
+            b = 7;
+            c = 12.5;
+            d = 16.7;
+            Console.WriteLine(" srednja vrijednost ovih brojeva je " + (a + b + c + d) / 4);
+            Console.ReadKey();
+        }
+    }
+}
